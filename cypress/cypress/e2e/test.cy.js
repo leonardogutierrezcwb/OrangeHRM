1b87d12dbf285617254710e3302ea254d9c339af
Leonardo Gutierrez <leonardogutierrezcwb@gmail.com>
2025-08-01 10:34:26 -0300

    updates

---
+++ cypress/cypress/e2e/test.cy.js
@@ -0,0 +1,32 @@
+/// <reference types="cypress" />
+
+//const SuperFakerBrasil = require('faker-brasil')
+
+describe('Acessar - Orange HRM', () => {
+  //const fakerBrasil = new SuperFakerBrasil()
+  it('CT - 001 - Login', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin') 
+    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123') 
+    cy.get('.oxd-form').contains('Login').click() 
+    cy.url().should('include', '/dashboard/index') 
+    cy.get('.oxd-userdropdown-tab > .oxd-icon').click() 
+    cy.get('.oxd-dropdown-menu').contains('Logout').click() 
+    cy.url().should('include', '/auth/login')    
+    cy.screenshot('Login - Realizado com sucesso') 
+    
+    
+  }),
+
+  it('CT - 002 - Credencial Inválida', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin') 
+    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin1231') 
+    cy.get('.oxd-form').contains('Login').click() 
+    cy.get('.oxd-alert').contains('Invalid credentials')
+    cy.screenshot('Login - Credencial inválida')
+    
+
+    
+  })
+})
