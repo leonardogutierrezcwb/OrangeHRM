3cf62e2e390d00aa68dc4340bdfe9139d182c9a2
Leonardo Gutierrez <leonardogutierrezcwb@gmail.com>
2025-08-05 14:57:34 -0300

    modified:   cypress/cypress/e2e/functional-tests.cy.js

--- cypress/cypress/e2e/functional-tests.cy.js
+++ cypress/cypress/e2e/functional-tests.cy.js
@@ -1,71 +1,67 @@
 /// <reference types="cypress" />
 
-var home = 'https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index'
-var login = 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'
-var username = 'Admin'
-var password = 'admin123'
-
 const { faker } = require('@faker-js/faker')
-var generatedPassword = faker.internet.password(8, true, /[A-Za-z0-9]/)
+
+const home = 'https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index'
+const login = 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'
+const username = 'Admin'
+const password = 'admin123'
 
 describe('Access a - Orange HRM', () => {
 
+  const generatedPassword = faker.internet.password(10, true)
+
   beforeEach(() => {
-    
     cy.login()
   })
 
   it('CT - Include - Admin', () => {
     cy.visit(home)
-    
+
     cy.get('.oxd-main-menu-item').contains('Admin').click()
     cy.url().should('include', '/admin/viewSystemUsers')
 
-    cy.get('.orangehrm-header-container').contains('Add').click()    
+    cy.get('.orangehrm-header-container').contains('Add').click()
     cy.url().should('include', '/admin/saveSystemUser')
 
-    cy.get(':nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-select-wrapper > .oxd-select-text > .oxd-select-text--after > .oxd-icon').click()
+    cy.get(':nth-child(1) > .oxd-input-group > :nth-child(2) .oxd-select-text').click()
     cy.get('.oxd-select-option').contains('Admin').click()
 
     cy.get('.oxd-autocomplete-text-input > input').type('ranga')
     cy.wait(1000)
     cy.get('.oxd-autocomplete-dropdown > :nth-child(1)').click()
 
-    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-select-wrapper > .oxd-select-text').click()
+    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) .oxd-select-text').click()
     cy.get('.oxd-select-option').contains('Enabled').click()
 
-    cy.get(':nth-child(4) > .oxd-input-group > :nth-child(2) > .oxd-input').type(faker.internet.username())
+    cy.get(':nth-child(4) > .oxd-input-group > :nth-child(2) > .oxd-input').type(generatedUsername)
     cy.get('.user-password-cell > .oxd-input-group > :nth-child(2) > .oxd-input').type(generatedPassword)
     cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type(generatedPassword)
-  
+
     cy.get('.oxd-form-actions').contains('Save').click()
     cy.get('.oxd-toast').should('contain', 'Successfully Saved')
 
     cy.screenshot('Admin - Add User')
-   // cy.clearCookies()
-   // cy.clearLocalStorage()
-    
-  }),
+  })
 
   it('CT - Create PIM - Employee', () => {
     cy.visit(home)
-    
+
     cy.get('.oxd-main-menu-item').contains('PIM').click()
     cy.url().should('include', '/pim/viewEmployeeList')
 
-    cy.get('.orangehrm-header-container').contains('Add').click()    
+    cy.get('.orangehrm-header-container').contains('Add').click()
     cy.url().should('include', '/pim/addEmployee')
 
     cy.get('.--name-grouped-field > :nth-child(1) > :nth-child(2) > .oxd-input').type(faker.name.firstName())
     cy.get(':nth-child(2) > :nth-child(2) > .oxd-input').type(faker.name.middleName())
     cy.get(':nth-child(3) > :nth-child(2) > .oxd-input').type(faker.name.lastName())
-  
+
     cy.get('.oxd-form-actions').contains('Save').click()
     cy.get('.oxd-toast').should('contain', 'Successfully Saved')
 
     cy.screenshot('PIM - Add Employee')
-   
-  }),
+  })
 
   it('CT - Create Recruitment - Candidate', () => {
     cy.visit(home)
@@ -82,15 +78,14 @@ describe('Access a - Orange HRM', () => {
 
     cy.get('.oxd-select-text').click()
     cy.get('.oxd-select-option').contains('Software Engineer').click()
-    cy.get(':nth-child(3) > .oxd-grid-3 > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-input')
-      .type(faker.internet.email())
-    
+
+    cy.get(':nth-child(3) > .oxd-grid-3 > :nth-child(1) .oxd-input').type(faker.internet.email())
     cy.get('.orangehrm-save-candidate-page-full-width > .oxd-input-group > :nth-child(2) > .oxd-input')
-      .type(faker.string.sample(10))
- 
+      .type(faker.lorem.sentence(3))
+
     cy.get('.oxd-form-actions').contains('Save').click()
     cy.get('.oxd-toast').should('contain', 'Successfully Saved')
-    cy.screenshot('Recruitment - Add Candidate')
 
+    cy.screenshot('Recruitment - Add Candidate')
   })
 })
